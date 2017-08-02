b0c6706879dba9bdbb7350aa633932208bf03f4f
Rajat Choudhury (BAS) <RA300349@wipro.com>
2017-08-02 12:22:03 +0530

    First Commit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,41 @@
+import { EventEmitter, Injectable } from "@angular/core";
+
+import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe(
+            'A Test Recipe', 
+            'This is a simple test', 'https://c.pxhere.com/photos/c1/96/sweet_struffoli_typical_sweet_dessert_food_recipe_kitchen_dish-754476.jpg!d',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe(
+            'Second Test Recipe', 
+            'This is a test', 'https://c.pxhere.com/photos/5f/6f/sweet_foods_food_sweet_biscuits_red_green_sugar_alimentari-964833.jpg!d',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 20)
+            ]),
+    ];
+
+    constructor(private slService: ShoppingListService){
+
+    }
+
+    getRecipes(){
+        return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.slService.addIngredients(ingredients)
+    }
+
+    
+}
